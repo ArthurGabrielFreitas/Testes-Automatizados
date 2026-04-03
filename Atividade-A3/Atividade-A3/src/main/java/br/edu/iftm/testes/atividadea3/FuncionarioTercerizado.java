b171eb1dffc8904ef698fc38e93064a84198dc00
Arthur Morais <artgabrielfdm@gmail.com>
2026-04-02 21:51:50 -0300

    Criando função para o teste de despesas acima do limite Ajuste no pom.xml

--- Atividade-A3/Atividade-A3/src/main/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizado.java
+++ Atividade-A3/Atividade-A3/src/main/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizado.java
@@ -1,5 +1,10 @@
 package br.edu.iftm.testes.atividadea3;
 
 public class FuncionarioTercerizado {
+
+    public Object setDespesasAdicionais(int valorDespesasAdicionais) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDespesasAdicionais'");
+    }
     
 }
--- Atividade-A3/Atividade-A3/src/test/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizadoTeste.java
+++ Atividade-A3/Atividade-A3/src/test/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizadoTeste.java
@@ -1,12 +1,24 @@
 package br.edu.iftm.testes.atividadea3;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class FuncionarioTercerizadoTeste {
+    private FuncionarioTercerizado funcionarioTercerizado;
+
     @BeforeEach
     void instanciarObjetoFuncionarioTercerizado(){
-        final FuncionarioTercerizado funcionarioTercerizado = new FuncionarioTercerizado();
+        funcionarioTercerizado = new FuncionarioTercerizado();
+    }
+
+    @Test
+    void testeModificarDespesaAcimaDoLimiteGeraErro() {
+        int valorDespesasAdicionais = 2000;
+
+        assertThrows(IllegalArgumentException.class, () -> funcionarioTercerizado.setDespesasAdicionais(valorDespesasAdicionais));
     }
 }
