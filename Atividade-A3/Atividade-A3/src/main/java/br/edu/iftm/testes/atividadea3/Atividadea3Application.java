e0d53db4de1a74862ce0b6225005296dd35e7212
Arthur Morais <artgabrielfdm@gmail.com>
2026-04-02 21:29:03 -0300

    Início da atividade A3

---
+++ Atividade-A3/Atividade-A3/src/main/java/br/edu/iftm/testes/atividadea3/Atividadea3Application.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.testes.atividadea3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividadea3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividadea3Application.class, args);
+	}
+
+}
---
+++ Atividade-A3/Atividade-A3/src/test/java/br/edu/iftm/testes/atividadea3/Atividadea3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.testes.atividadea3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividadea3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
