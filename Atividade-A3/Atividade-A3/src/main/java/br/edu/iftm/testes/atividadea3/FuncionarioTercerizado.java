e2e95081b6406369d80a9ac6cef36c0449c8f059
Arthur Morais <artgabrielfdm@gmail.com>
2026-04-02 22:13:06 -0300

    Criando teste de erro de pagamento com despesas adicionais acima do limite Ajustes de tipagem no teste anterior

--- Atividade-A3/Atividade-A3/src/main/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizado.java
+++ Atividade-A3/Atividade-A3/src/main/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizado.java
@@ -2,9 +2,24 @@ package br.edu.iftm.testes.atividadea3;
 
 public class FuncionarioTercerizado {
 
-    public Object setDespesasAdicionais(int valorDespesasAdicionais) {
+    public Object setDespesasAdicionais(Double valorDespesasAdicionais) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setDespesasAdicionais'");
+    }
+
+    public Object calcularPagamento() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calcularPagamento'");
+    }
+
+    public void setValorHora(Double valorHora) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setValorHora'");
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setHorasTrabalhadas'");
     }
     
 }
--- Atividade-A3/Atividade-A3/src/test/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizadoTeste.java
+++ Atividade-A3/Atividade-A3/src/test/java/br/edu/iftm/testes/atividadea3/FuncionarioTercerizadoTeste.java
@@ -17,8 +17,20 @@ public class FuncionarioTercerizadoTeste {
 
     @Test
     void testeModificarDespesaAcimaDoLimiteGeraErro() {
-        int valorDespesasAdicionais = 2000;
+        Double valorDespesasAdicionais = 2000.0;
 
         assertThrows(IllegalArgumentException.class, () -> funcionarioTercerizado.setDespesasAdicionais(valorDespesasAdicionais));
+    }
+
+    @Test
+    void testeModificarDespesaGeraPagamentoAcimaDoLimiteDeSalarioGerandoErro() {
+        Double valorDespesasAdicionais = 1000.0;
+        Double valorHora = 60.0;
+        int horasTrabalhadas = 160;
+
+        funcionarioTercerizado.setDespesasAdicionais(valorDespesasAdicionais);
+        funcionarioTercerizado.setValorHora(valorHora);
+        funcionarioTercerizado.setHorasTrabalhadas(horasTrabalhadas);
+        assertThrows(IllegalArgumentException.class, () -> funcionarioTercerizado.calcularPagamento());
     }
 }
