45b47589d9a7ea52a24cfc4aa854818bce19ef7e
Arthur Morais <artgabrielfdm@gmail.com>
2026-04-20 18:47:58 -0300

    A4 - Ciclo 2 - GREEN

--- projeto-veterinario-springboot-h2/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
+++ projeto-veterinario-springboot-h2/gerenciador-Veterinarios/src/main/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface VeterinarioRepository extends JpaRepository<Veterinario, Integer> {
 
+   @Query("SELECT v FROM Veterinario v WHERE LOWER(v.nome) LIKE LOWER(CONCAT('%',:nome,'%'))")
+   public List<Veterinario> findByNomeContains(String nome);
 
    @Query("SELECT v FROM Veterinario v WHERE LOWER(v.nome) = LOWER(:nome)")
    public List<Veterinario> findByNomeIgnoreCase(String nome);
