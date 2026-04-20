078d12af99c4dc000fd58ff8a871076f8205aa2b
Arthur Morais <artgabrielfdm@gmail.com>
2026-04-20 18:06:36 -0300

    A4 - Ciclo 1 - RED

--- projeto-veterinario-springboot-h2/gerenciador-Veterinarios/src/test/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepositoryTest.java
+++ projeto-veterinario-springboot-h2/gerenciador-Veterinarios/src/test/java/org/iftm/gerenciadorveterinarios/repositories/VeterinarioRepositoryTest.java
@@ -1,6 +1,12 @@
 package org.iftm.gerenciadorveterinarios.repositories;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.iftm.gerenciadorveterinarios.entities.Veterinario;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,7 +17,22 @@ public class VeterinarioRepositoryTest {
     private VeterinarioRepository repositorio;
 
     @Test
-    void testFindByNomeContains() {
-        fail("Não implementado ainda");
+    void testeBuscarPorNomeIgnorandoCase() {
+        String nomeBusca = "ANA LUIZA BORGES";
+        String nomeEsperado = "Ana Luiza Borges";
+
+        List<Veterinario> resultado = repositorio.findByNomeIgnoreCase(nomeBusca);
+
+        assertFalse(resultado.isEmpty(), "A lista não deveria estar vazia");
+        assertEquals(nomeEsperado, resultado.get(0).getNome());
+    }
+
+    @Test
+    void testeBuscarPorNomeInexistente() {
+        String nomeBusca = "Zé Ninguém";
+
+        List<Veterinario> resultado = repositorio.findByNomeIgnoreCase(nomeBusca);
+
+        assertTrue(resultado.isEmpty(), "A lista deveria estar vazia");
     }
 }
